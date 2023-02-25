a276b0dcac12102142f27e7c3ffb5d9e0c520b9b
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2023-02-24 20:28:56 -0500

    flask server scaffolding

--- flask/server.py
+++ flask/server.py
@@ -8,8 +8,19 @@ def login():
     email = request.json["email"]
     password = request.json["password"]
 
-    data = {"email": email, 
-        "password": password}
+    data = {"email": email + "hoeeeeeeeeeeee", 
+        "password": password + "hoeeeeeeeeeeee"}
 
     return json.dumps(data)
 
+@app.after_request
+def after_request(response):
+    white_origin = ['http://localhost:3000']
+    if request.headers['Origin'] in white_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
+        response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
